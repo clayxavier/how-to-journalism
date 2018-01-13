311fceb9fe585bfe84d49e1e761ac302680a5022
clayxavier <clayxavier@alldayplay.fm>
2018-01-12 18:01:26 -0800

    Style Changes

---
+++ journalism.js
@@ -0,0 +1,7 @@
+document.getElementById("correct").onClick.classList.add('clicked');
+
+document.getElementById("MyElement").classList.remove('MyClass');
+
+if ( document.getElementById("MyElement").classList.contains('MyClass') )
+
+document.getElementById("MyElement").classList.toggle('MyClass');
