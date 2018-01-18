46076dd36efd8746bdd035a886d1e45b18ae94c9
clayxavier <clayxavier@alldayplay.fm>
2018-01-18 14:57:47 -0800

    Merged Master Branch with JS branch

--- journalism.js
+++ journalism.js
@@ -1,7 +1,149 @@
-document.getElementById("correct").onClick.classList.add('');
+//Initial state
 
-document.getElementById("MyElement").classList.remove('MyClass');
+$(document).ready(function() {
+	$("#questionTwo,#questionThree,#questionFour,#questionFive,#questionSix,#questionSeven,#questionEight").hide();
 
-if ( document.getElementById("MyElement").classList.contains('MyClass') )
+});
 
-document.getElementById("MyElement").classList.toggle('MyClass');
+
+//Correct scripts, determines if question was answered correctly
+
+$("#questionOne").find('#correct').one( 'click', (function() {
+		console.log("Correct clicked!");
+		$("#inner1").replaceWith("Answer correct! ");
+		$("#questionOne").find('#correct').css("background-color","green");
+		$("#questionTwo").show();
+		$('html, body').animate({
+	        scrollTop: $("#questionTwo").offset().top
+	    }, 500);
+				}
+	    )
+	);
+
+$("#questionTwo").find('#correct').one( 'click', (function() {
+	console.log("Correct clicked!");
+	$("#inner2").replaceWith("Answer correct! ");
+	$("#questionTwo").find('#correct').css("background-color","green");
+	$("#questionThree").show();
+	$('html, body').animate({
+        scrollTop: $("#questionThree").offset().top
+    }, 500);
+}));
+
+$("#questionThree").find('#correct').one( 'click', (function() {
+	console.log("Correct clicked!");
+	$("#inner3").replaceWith("Answer correct! ");
+	$("#questionThree").find('#correct').css("background-color","green");
+	$("#questionFour").show();
+	$('html, body').animate({
+        scrollTop: $("#questionFour").offset().top
+    }, 500);
+}));
+
+$("#questionFour").find('#correct').one( 'click', (function() {
+	console.log("Correct clicked!");
+	$("#inner4").replaceWith("Answer correct! ");
+	$("#questionFour").find('#correct').css("background-color","green");
+	$("#questionFive").show();
+	$('html, body').animate({
+        scrollTop: $("#questionFive").offset().top
+    }, 500);
+}));
+
+$("#questionFive").find('#correct').one( 'click', (function() {
+	console.log("Correct clicked!");
+	$("#inner5").replaceWith("Answer correct! ");
+	$("#questionFive").find('#correct').css("background-color","green");
+	$("#questionSix").show();
+	$('html, body').animate({
+        scrollTop: $("#questionSix").offset().top
+    }, 500);
+}));
+
+$("#questionSix").find('#correct').one ( 'click', (function() {
+	console.log("Correct clicked!");
+	$("#inner6").replaceWith("Answer correct! ");
+	$("#questionSix").find('#correct').css("background-color","green");
+	$("#questionSeven").show();
+	$('html, body').animate({
+        scrollTop: $("#questionSeven").offset().top
+    }, 500);
+}));
+
+$("#questionSeven").find('#correct').one ( 'click', (function() {
+	console.log("Correct clicked!");
+	$("#inner7").replaceWith("Answer correct!" );
+	$("#questionSeven").find('#correct').css("background-color","green");
+	$("#questionEight").show();
+	$('html, body').animate({
+        scrollTop: $("#questionEight").offset().top
+    }, 500);
+}));
+
+$("#questionEight").find('#correct').one ( 'click', (function() {
+	console.log("Correct clicked!");
+	$("#inner8").replaceWith("Answer correct! ");
+	$("#questionEight").find('#correct').css("background-color","green");
+	$("#final").show();
+	$('html, body').animate({
+        scrollTop: $("#final").offset().top
+    }, 500);
+}));
+
+//Incorrect scripts, determines if question was answered incorrectly
+
+$("#questionOne").find('#wrong').one( 'click', (function() {
+		console.log("Wrong clicked!");
+		$("#inner1").append("Wrong answer! ");
+				}
+	    )
+	);
+
+$("#questionTwo").find('#wrong').one( 'click', (function() {
+		console.log("Wrong clicked!");
+		$("#inner2").append("Wrong answer! ");
+				}
+	    )
+	);
+
+$("#questionThree").find('#wrong').one( 'click', (function() {
+		console.log("Wrong clicked!");
+		$("#inner3").append("Wrong answer! ");
+				}
+	    )
+	);
+
+$("#questionFour").find('#wrong').one( 'click', (function() {
+		console.log("Wrong clicked!");
+		$("#inner4").append("Wrong answer! ");
+				}
+	    )
+	);
+
+$("#questionFive").find('#wrong').one( 'click', (function() {
+		console.log("Wrong clicked!");
+		$("#inner5").append("Wrong answer! ");
+				}
+	    )
+	);
+
+$("#questionSix").find('#wrong').one( 'click', (function() {
+		console.log("Wrong clicked!");
+		$("#inner6").append("Wrong answer! ");
+				}
+	    )
+	);
+
+$("#questionSeven").find('#wrong').one( 'click', (function() {
+		console.log("Wrong clicked!");
+		$("#inner7").append("Wrong answer! ");
+				}
+	    )
+	);
+
+$("#questionEight").find('#wrong').one( 'click', (function() {
+		console.log("Wrong clicked!");
+		$("#inner8").append("Wrong answer! ");
+				}
+	    )
+	);
